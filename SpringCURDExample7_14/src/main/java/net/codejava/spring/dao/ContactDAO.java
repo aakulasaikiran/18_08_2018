ec278835347029002cea58cb2e7d78426e8345da
aakulasaikiran <aakulasaikiran@gmail.com>
2018-08-18 17:08:17 +0530

    cc

---
+++ SpringCURDExample7_14/src/main/java/net/codejava/spring/dao/ContactDAO.java
@@ -0,0 +1,23 @@
+package net.codejava.spring.dao;
+
+import java.util.List;
+
+import net.codejava.spring.model.Contact;
+
+/**
+ * Defines DAO operations for the contact model.
+ * 
+ * @author www.codejava.net
+ *
+ */
+public interface ContactDAO {
+
+	public void saveOrUpdate(Contact contact);
+
+	
+	  public void delete(int contactId);
+	  
+	  public Contact get(int contactId,Contact contact);
+	 
+	public List<Contact> list();
+}
